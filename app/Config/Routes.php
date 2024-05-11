28d8d6ef526ee728334639140e5b3116b0e22dc6
Luiz Fernando <luisferndantas@outlook.com>
2024-05-11 17:04:51 -0300

    inicio

---
+++ app/Config/Routes.php
@@ -0,0 +1,21 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->group('User', function (RouteCollection $routes) {
+    $routes->get(
+        'usuarios',
+        'UsuarioController::listUsers',
+        ['filter' => 'token']
+    );
+    $routes->post(
+        'create-user',
+        'UsuarioController::createUser',
+        ['filter' => 'token']
+    );
+});
---
+++ app/Controllers/UsuarioController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controllers;
+
+
+use CodeIgniter\RESTful\ResourceController;
+use App\Models\UsuarioModel;
+
+
+class UsuarioController extends ResourceController
+{
+    private $usuario;
+
+    public function __construct()
+    {
+        $this->usuario = new UsuarioModel();
+    }
+
+    private function _verificarUsuario($email){
+        // checa se o email ja está em uso
+        $usuario = $this->usuario->getUserByEmail($email);
+        if(empty($usuario)){
+            return true;
+    }else{
+        return false;
+    }}
+    /**
+     * createUser
+     * 
+     * @method post
+     * 
+     * @return json
+     */
+    public function createUser(){
+        $response = [];
+
+        $email = (string) $this->request->getPost("email");
+        $verify = $this->_verificarUsuario($email);
+
+        if($verify){
+            try{
+                $newUser = [
+                    'nome_usuario' => $this->request->getPost('nome'),
+                    'email_usuario' => $this->request->getPost('email'),
+                    'senha_usuario' => $this->request->getPost('senha'),
+                    'cargo_usuario' => empty($this->request->getPost('cargo')) ? null : $this->request->getPost('cargo'),
+                    'id_grupo' => empty($this->request->getPost('grupo')) ? null : $this->request->getPost('grupo') ,
+                    'is_admin' => empty($this->request->getPost('is_admin')) ? 0 : $this->request->getPost('is_admin'),
+                    'is_leader'=> empty($this->request->getPost('is_leader')) ? 0 : $this->request->getPost('is_leader'),
+                    'is_ceo'=> empty($this->request->getPost('is_ceo')) ? 0 : $this->request->getPost('is_leader')
+                ];
+
+                $user_criado = $this->usuario->create($newUser);
+
+                if($user_criado == 0){
+                    $response = [
+                        "status"=> "success",
+                        "message"=> "O usuario foi criado com sucesso!"
+                    ];
+                }
+            }catch(\Exception $e){
+                $response = [
+                    "status"=> "error",
+                    "message"=> "O usuario não foi criado, cheque os dados!",
+                    "error" => $e->getMessage()
+                ];
+            }
+        }else{
+            $response = [
+                "status"=> 'error',
+                'message'=> 'Usuario com email ja existe!'
+            ];
+        }
+        echo json_encode($response);
+        exit;
+    }
+    /**
+     * createUser
+     * 
+     * @method post
+     * 
+     * @return json
+     */
+    public function listUsers()
+    {
+        $users = $this->usuario->list();
+        echo json_encode( $users );
+        exit;
+    }
+}

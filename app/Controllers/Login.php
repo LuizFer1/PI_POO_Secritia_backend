929ea320750b8076d904c47188369a309213d26d
Luiz Fernando <luisferndantas@outlook.com>
2024-05-22 14:37:49 -0300

    fix: login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\UsuarioModel;
 use CodeIgniter\Controller;
 
 class Login extends Controller
@@ -13,16 +14,26 @@ class Login extends Controller
 
     public function process()
     {
-        $email = $this->request->getPost('email_usuario');
-        $password = (string)$this->request->getPost('senha_usuario');
-
-        $userModel = (new \App\Models\UsuarioModel)->login($email, $password);
-
-        if ($userModel) {
-                return redirect()->to('login')->with('success','Usuario logado');
-        }else {
-            // Usuário não encontrado, redirecione de volta para o formulário de login com uma mensagem de erro
-            return redirect()->to('login')->with('error', 'Usuário não encontrado. Por favor, verifique o seu e-mail e tente novamente.');
+        $validate = $this->validate(
+            [
+                'email'=> 'required|valid_email',
+                'password'=> 'required'
+            ],
+        );
+        if(!$validate){
+            return redirect()->route('login')->with('error',$this->validator->getErrors());
+        }        
+        $user = new UsuarioModel();
+        $userFound = $user->where('email_usuario', $this->request->getPost('email'))->first();
+        if(!$userFound){
+            return redirect()->route('login')->with('error','Email or password incorrect!');
         }
-    }
+        if(!password_verify($this->request->getPost('password'), $userFound->senha_usuario)){
+            return redirect()->route('login')->with('error','Email or password gt incorrect!');
+        }
+        unset( $userFound->senha_usuario );
+        session()->set('user', $userFound );
+        
+        return redirect()->route('home');
+    }  
 }
